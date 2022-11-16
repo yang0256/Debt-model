75bd2163fd633a450ca2d2e05382a909dc395b83
Tao Yang <115559820+yang0256@users.noreply.github.com>
2022-11-15 19:14:07 -0500

    Add files via upload

---
+++ import_data.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 17:05:48 2022
+
+@author: solvm
+"""
+
+import pandas as pd
+data = pd.read_csv('C:\IMPCH.csv')
+# print(print(data.iloc[:]))
+print(data.iloc[:])
+
+data = pd.read_csv('C:\MICH.csv')
+# print(print(data.iloc[:]))
+print(data.iloc[:])
+
+
+data = pd.read_csv('C:\data-DGS10-20-5-2-30-3-1.csv')
+# print(print(data.iloc[:]))
+print(data.iloc[:])
+
+
+
